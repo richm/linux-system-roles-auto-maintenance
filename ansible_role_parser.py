eac493787d8c0fcf762a5e97f319a3fccb51c894
Rich Megginson <rmeggins@redhat.com>
2020-09-08 18:07:51 -0600

    wip

--- ansible_role_parser.py
+++ ansible_role_parser.py
@@ -17,14 +17,6 @@ if os.environ.get("LSR_DEBUG") == "true":
 ROLE_DIRS = ["defaults", "examples", "files", "handlers", "library", "meta",
     "module_utils", "tasks", "templates", "tests", "vars"]
 
-def is_role_dir(role_path, dirpath):
-    if role_path == dirpath:
-        return False
-    dir_pth = Path(dirpath)
-    relpath = dir_pth.relative_to(role_path)
-    base_dir = relpath.parts[0]
-    return base_dir in ROLE_DIRS
-
 PLAY_KEYS = {
     "gather_facts",
     "handlers",
@@ -36,6 +28,14 @@ PLAY_KEYS = {
     "tasks",
 }
 
+
+def is_role_dir(role_path, dirpath):
+    if role_path == dirpath:
+        return False
+    dir_pth = Path(dirpath)
+    relpath = dir_pth.relative_to(role_path)
+    base_dir = relpath.parts[0]
+    return base_dir in ROLE_DIRS
 
 def get_file_type(item):
     if isinstance(item, AnsibleMapping):
@@ -68,7 +68,7 @@ def handle_other(item):
 def handle_vars(item):
     """handle vars of Ansible item"""
     for var in item.get("vars", []):
-        print(f"\tvar = {var}")
+        logging.debug(f"\tvar = {var}")
     return
 
 def handle_meta(item):
@@ -76,7 +76,7 @@ def handle_meta(item):
     for role in item.get("dependencies", []):
         print(f"\tmeta dependencies role {role}")
 
-def handle_task(task):
+def handle_task(task, role_modules):
     """handle a single task"""
     mod_arg_parser = ModuleArgsParser(task)
     try:
@@ -87,35 +87,40 @@ def handle_task(task):
         print(f"\ttask role {task[action]['name']}")
     elif action in role_modules:
         print(f"\ttask role module {action}")
-    handle_tasks(task)
+    handle_tasks(task, role_modules)
 
-def handle_task_list(tasks):
+def handle_task_list(tasks, role_modules):
     """item is a list of Ansible Task objects"""
     for task in tasks:
         if "block" in task:
-            handle_tasks(task)
+            handle_tasks(task, role_modules)
         else:
-            handle_task(task)
+            handle_task(task, role_modules)
     
-def handle_tasks(item):
+def handle_tasks(item, role_modules):
     """item has one or more fields which hold a list of Task objects"""
     if "always" in item:
-        handle_task_list(item["always"])
+        handle_task_list(item["always"], role_modules)
     if "block" in item:
-        handle_task_list(item["block"])
+        handle_task_list(item["block"], role_modules)
     if "handlers" in item:
-        handle_task_list(item["post_tasks"])
+        handle_task_list(item["post_tasks"], role_modules)
     if "pre_tasks" in item:
-        handle_task_list(item["pre_tasks"])
+        handle_task_list(item["pre_tasks"], role_modules)
     if "post_tasks" in item:
-        handle_task_list(item["post_tasks"])
+        handle_task_list(item["post_tasks"], role_modules)
     if "rescue" in item:
-        handle_task_list(item["rescue"])
+        handle_task_list(item["rescue"], role_modules)
     if "tasks" in item:
-        handle_task_list(item["tasks"])
+        handle_task_list(item["tasks"], role_modules)
 
-def use_ansible():
-    role_path = sys.argv[1]
+def parse_role(role_path):
+    role_modules = set()
+    library_path = Path(os.path.join(role_path, "library"))
+    if library_path.is_dir():
+        for mod_file in library_path.iterdir():
+            if mod_file.is_file() and mod_file.stem != "__init__":
+                role_modules.add(mod_file.stem)
     for (dirpath, _, filenames) in os.walk(role_path):
         if not is_role_dir(role_path, dirpath):
             continue
@@ -126,6 +131,9 @@ def use_ansible():
             print(f"filepath {filepath}")
             dl = DataLoader()
             ans_data = dl.load_from_file(filepath)
+            if ans_data is None:
+                print(f"file is empty {filepath}")
+                continue
             file_type = get_file_type(ans_data)
             if file_type == "vars":
                 handle_vars(item)
@@ -135,10 +143,11 @@ def use_ansible():
                 continue
             for item in ans_data:
                 ans_type = get_item_type(item)
-                handle_tasks(item)
                 handle_vars(item)
                 handle_other(item)
                 if ans_type == "task":
-                    handle_task(item)
+                    handle_task(item, role_modules)
+                handle_tasks(item, role_modules)
 
-use_ansible()
+for role_path in sys.argv[1:]:
+    parse_role(role_path)
